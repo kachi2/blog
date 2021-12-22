05ffb68d5a213dfaf9ce6dc9b75e94b5901e380f
Kachi <mac@Macs-MacBook-Pro.local>
2021-12-23 00:10:30 +0100

    from mac

--- app/resources/views/layouts/app.blade.php
+++ app/resources/views/layouts/app.blade.php
@@ -6,313 +6,137 @@
 	<meta name="author" content="">
 	<meta name="keywords" content="">
 	<meta name="description" content="Crypto News blog is a cryptocurrency news service that covers latest bitcoin news today, technical analysis &amp; price for bitcoin and other altcoins">
-
     <!-- CSRF Token -->
     <meta name="csrf-token" content="{{ csrf_token() }}">
-
     <title>{{ config('app.name', 'cryptonews') }}</title>
-
     <!-- Scripts -->
-    <script src="{{ asset('js/app.js') }}" defer></script>
-
-    <!-- Fonts -->
     <link rel="dns-prefetch" href="//fonts.gstatic.com">
    	<link href="https://fonts.googleapis.com/css?family=Roboto:300,400,500,600,700%7CSource+Sans+Pro:200,300,400,500,600,700,900" rel="stylesheet">
 
-	
-
     <!-- Styles -->
-    <link href="{{ asset('css/app.css') }}" rel="stylesheet">
-
-    <link rel="stylesheet" href="{{asset('/assets/font/demo-files/demo.css')}}">
-
-	<!-- CSS theme files
-	============================================ -->
-	<link rel="stylesheet" href="{{asset('/assets/css/bootstrap-grid.min.css')}}">
-	<link rel="stylesheet" href="{{asset('/assets/css/fontello.css')}}">
-	<link rel="stylesheet" href="{{asset('/assets/css/owl.carousel.css')}}">
-	<link rel="stylesheet" href="{{asset('/assets/css/style.css')}}">
-	<link rel="stylesheet" href="{{asset('/assets/css/responsive.css')}}">
+  <link href="{{asset('/assets/img/favicon.png')}}" rel="icon">
+  <link href="{{asset('/assets/css/bootstrap.min.css')}}" rel="stylesheet">
+  <link href="{{asset('/assets/css/animsition.min.css')}}" rel="stylesheet">
+  <link href="{{asset('/assets/css/font-awesome.min.css')}}" rel="stylesheet">
+  <link href="{{asset('/assets/themify-icons/themify-icons.css')}}" rel="stylesheet">
+  <link href="{{asset('/assets/css/bootsnav.css')}}" rel="stylesheet">
+  <link href="{{asset('/assets/owl-carousel/owl.carousel.css')}}" rel="stylesheet">
+  <link href="{{asset('/assets/owl-carousel/owl.theme.css')}}" rel="stylesheet">
+  <link href="{{asset('/assets/owl-carousel/owl.transitions.css')}}" rel="stylesheet">
+  <link href="{{asset('/assets/css/magnific-popup.css')}}" rel="stylesheet">
+  <link href="{{asset('/assets/css/fluidbox.min.css')}}" rel="stylesheet">
+  <link href="{{asset('/assets/css/style.css')}}" rel="stylesheet">
 </head>
-<body>
-  <div class="loader"></div>
-
-
-
-  <!-- - - - - - - - - - - - - - Wrapper - - - - - - - - - - - - - - - - -->
-
-  <div id="wrapper" class="wrapper-container">
-
-    <!-- - - - - - - - - - - - - Mobile Menu - - - - - - - - - - - - - - -->
-
-    <nav id="mobile-advanced" class="mobile-advanced"></nav>
-
-    <!-- - - - - - - - - - - - - - Header - - - - - - - - - - - - - - - - -->
-
-    <header id="header" class="header sticky-header">
-      
-      <!-- searchform -->
-
-      <div class="searchform-wrap">
-        <div class="vc-child h-inherit">
-
-          <form class="search-form">
-            <button type="submit" class="search-button"></button>
-            <div class="wrapper">
-              <input type="text" name="search" placeholder="Start typing...">
-            </div>
-          </form>
-
-          <button class="close-search-form"></button>
-
-        </div>
-      </div>
-      
-      <!-- pre-header -->
-
-      <div class="pre-header">
-        
-        <div class="container">
-          
-          <div class="row justify-content-between">
-            
-            <div class="col">
-              
-              <div class="date" style="color:#fff">
-               {{Date('D, M d, Y')}}
+<body class="blog-layout">
+  <div class="main-content animsition">
+      <div class="page-outer-wrap">
+          <div class="top-header">
+              <div class="container">
+                  <div class="row">
+                      <div class="col-sm-6 col-md-7">
+                          <div class="header-nav">
+                              <ul>
+                                  <li><span class="headre-weather"><i class="fa fa-calendar-check-o"></i>&nbsp; Thursday, January 25</span></li>
+                                  <li><a href="contact.html">Contact</a></li>
+                                  <li><a href="about-me.html">about</a></li>
+                                  <li><a href="#">Buy now!</a></li>
+                              </ul>
+                          </div>
+                      </div>
+                      <div class="col-sm-6 col-md-5">
+                          <ul class="top-socia-share">
+                              <li>
+                                  <a href="#"><i class="fa fa-facebook"></i></a>
+                                  <a href="#"><i class="fa fa-twitter"></i></a>
+                                  <a href="#"><i class="fa fa-instagram"></i></a>
+                                  <a href="#"><i class="fa fa-linkedin"></i></a>
+                                  <a href="#"><i class="fa fa-youtube-play"></i></a>
+                              </li>
+                              <li>
+                                  <a href="javascript:void(0)" data-toggle="modal" data-target="#user-modal">Login / Register</a>
+                              </li>
+                              <li>
+                                  <div class="weather-top">
+                                      <i class="fa fa-cloud"></i>
+                                      <div class="weather-now">
+                                          <span class="degrees">- 5.9</span>
+                                          <span class="unit">C</span>
+                                      </div>
+                                      <div class="weather-city">New York</div>
+                                  </div>
+                              </li>
+                          </ul>
+                      </div>
+                  </div>
               </div>
-
-            </div>
-
-            <div class="col align-right">
-
-              <div class="lang-area">
-
-                <a href="#">English</a>
-               
-
-              </div>
-
-            </div>
-
           </div>
-
-        </div>
-
-      </div>
-
-      <!-- top-header -->
-
-      <div class="top-header">
-
-        <div class="container">
-
-          <div class="row justify-content-between align-items-center">
-
-
-                  <div class="col">
-              
-              <!-- logo -->
-
-              <div class="logo-wrap">
-
-                <a href="index.html" class="logo"><img src="{{asset('/assets/images/logo.png')}}" alt=""></a>
-
-              </div>
-
-            </div>
-            <div class="col">
-              
-              <!-- Socials -->
-
-              <ul class="social-icons">
-
-                <li><a href="#"><i class="icon-facebook"></i></a></li>
-                <li><a href="#"><i class="icon-twitter"></i></a></li>
-                <li><a href="#"><i class="icon-rss"></i></a></li>
-                <li><a href="#"><i class="icon-hash"></i></a></li>
-
-              </ul>
-
-            </div>
-            
-          
-
-            <div class="col align-right">
-              <a href="#" class="btn btn-style-4 btn-big"><i class="licon-mailbox-full"></i>Subscribe</a>
-            </div>
-          </div>
-        </div>
-      </div>
-      
-      <!-- - - - - - - - - - - - / Mobile Menu - - - - - - - - - - - - - -->
-
-      <!--main menu-->
-
-      <div class="menu-holder">
-        <div class="container">
-          <div class="menu-wrap">
-            <div class="nav-item">
-              <!-- - - - - - - - - - - - - - Navigation - - - - - - - - - - - - - - - - -->
-              <nav id="main-navigation" class="main-navigation">
-                <ul id="menu" class="clearfix">
-                  <li class="dropdown"><a href="#">Home</a>
-                    <!--sub menu-->
-                    <div class="sub-menu-wrap">
-                      <ul>
-                        <li><a href="index.html">Home - Blog</a></li>
-                        <li><a href="home_2.html">Home - Corporate</a></li>
-                      </ul>
-                    </div>
-                  </li>
-                  <li class="dropdown"><a href="#">ICO Calendar</a>
-                    <!--sub menu-->
-                    <div class="sub-menu-wrap">
-                      <ul>
-                        <li><a href="ico_calendar.html">ICO List</a></li>
-                        <li><a href="ico_single.html">Single ICO Page</a></li>
-                      </ul>
-                    </div>
-                  </li>
-                </ul>
-              </nav>
-              <!-- - - - - - - - - - - - - end Navigation - - - - - - - - - - - - - - - -->
-            </div>
-
-            <div class="search-holder">
-              <button type="button" class="search-button"></button>
-            </div>
-          </div>
-        </div>
-      </div>
-    </header>
-    <footer id="footer" class="footer">
-      <!-- top footer -->
-      <div class="top-footer">
-        <div class="container">
-          <div class="row align-items-center">
-            <div class="col-lg-3 col-md-12">
-              <a href="index.html" class="logo"><img src="images/logo2.png" alt=""></a>
-            </div>
-            <div class="col-lg-6 col-md-12 align-center">
-              <ul class="menu-list">
-                <li><a href="#">Write for Us</a></li>
-                <li><a href="#">Terms & Conditions</a></li>
-                <li><a href="#">Privacy Policy</a></li>
-                <li><a href="#">Contact</a></li>
-                <li><a href="#">Register</a></li>
-              </ul>
-            </div>
-            <div class="col-lg-3 col-md-12 align-right">
-              <ul class="social-icons">
-                <li><a href="#"><i class="icon-facebook"></i></a></li>
-                <li><a href="#"><i class="icon-twitter"></i></a></li>
-                <li><a href="#"><i class="icon-rss"></i></a></li>
-                <li><a href="#"><i class="icon-hash"></i></a></li>
-              </ul>
-            </div>
-          </div>
-        </div>
-      </div>
-
-    @yield('content')
-
-
-      <!-- main footer -->
-      <div class="main-footer">
-        
-        <div class="container">
-          
-          <div class="row">
-
-            <div class="col-lg-3 col-md-6 col-sm-12">
-              
-              <div class="widget">
-                
-                <h6 class="widget-title">About</h6>
-
-                <p>Nemo enim ipsam voluptatem quia voluptas aut fugit, sed quia consequuntur magni dolores eos qui ratione.</p>
-                <p>Est, qui dolorem ipsum quia dolor sit amet, nsectetur, sed quia non numquam eius modi tempora incidunt ut labore. </p>
-
-              </div>
-
-            </div>
-            <div class="col-lg-3 col-md-6 col-sm-12">
-              
-              <div class="widget">
-                
-                <h6 class="widget-title">Live Charts</h6>
-
-                <ul class="chart-list">
-                
-                  <li><a href="#">Bitcoin Price</a></li>
-                  <li><a href="#">Bitcoin Cash Price</a></li>
-                  <li><a href="#">Bitcoin Gold Price</a></li>
-                  <li><a href="#">Ethereum Price</a></li>
-                  <li><a href="#">Ethereum Classic Price</a></li>
-                  <li><a href="#">Litecoin Price</a></li>
-
-                </ul>
-
-              </div>
-
-            </div>
-            <div class="col-lg-3 col-md-6 col-sm-12">
-              
-              <div class="widget">
-                
-                <h6 class="widget-title">Newsletter Sign Up</h6>
-                
-                <p>Subscribe now and get exclusive news, interviews and stories.</p>
-                <form id="newsletter" class="newsletter">
-                  <input type="email" name="newsletter-email" placeholder="Enter your email address">
-                  <button type="submit" data-type="submit" class="btn btn-style-4"><i class="licon-mailbox-full"></i>Subscribe</button>
-                </form>
-
-              </div>
-
-            </div>
-            <div class="col-lg-3 col-md-6 col-sm-12">
-              
-              <div class="widget">
-                
-                <div class="banner-title">Advertisement</div>
-
-                <a href="#" class="banner"><img src="images/300x250_banner.jpg" alt=""></a>
-
-              </div>
-
-            </div>
-
-          </div>
-
-        </div>
-
-      </div>
-
-      <div class="copyright">
-          
-        <p>Copyright © 2018 Cryptox. All Rights Reserved.</p>
-
-      </div>
-
-    </footer>
-
-    <!-- - - - - - - - - - - - - end Footer - - - - - - - - - - - - - - - -->
-
-  </div>
-
-  <!-- - - - - - - - - - - - end Wrapper - - - - - - - - - - - - - - -->
-  <script src="{{asset('/assets/js/libs/jquery.modernizr.js')}}"></script>
-  <script src="{{asset('/assets/js/libs/jquery-2.2.4.min.js')}}"></script>
-  <script src="{{asset('/assets/js/libs/jquery-ui.min.js')}}"></script>
-  <script src="{{asset('/assets/js/libs/retina.min.js')}}"></script>
-  <script src="{{asset('/assets/plugins/jquery.queryloader2.min.js')}}"></script>
-  <script src="{{asset('/assets/plugins/owl.carousel.min.js')}}"></script>
-  <!-- JS theme files
-  ============================================ -->
-  <script src="{{asset('/assets/js/plugins.js')}}"></script>
-  <script src="{{asset('/assets/js/script.js')}}"></script>
+          <!-- /.End of top header -->
   
+
+            @yield('content')
+
+
+          <footer class="footer-2">
+            <div class="container">
+                <div class="footer-inner">
+                    <a href="#" class="footer-logo"><i class="ico-logo-footer"></i></a>
+                    <!-- /.footer Loog -->
+                    <div class="footer-copyright">
+                        <p>All rights reserved © 2018 The Osru</p>
+                        <p><a href="#">Terms and Conditions of Use</a></p>
+                    </div>
+                    <!-- /.footer Copyright -->
+                    <div class="footer-credits">
+                        <p>Design:<a target="_blank" href="#">bdtask </a></p>
+                    </div>
+                    <!-- /.footer Credits -->
+                </div>
+            </div>
+        </footer>
+    </div>
+
+
+
+
+
+  <!-- - - - - - - - - - - - JS files - - - - - - - - - - - - - - -->
+ 
+       <script src="{{asset('/assets/js/jquery.min.js')}}"></script>
+        <script src="{{asset('/assets/js/bootstrap.min.js')}}"></script>
+        <script src="{{asset('/assets/js/animsition.min.js')}}"></script>
+        <script src="{{asset('/assets/js/bootsnav.js')}}"></script>
+        <script src="{{asset('/assets/js/macy.js')}}"></script>
+        <script src="{{asset('/assets/js/imagesloaded.pkgd.min.js')}}"></script>
+        <script src="{{asset('/assets/js/ResizeSensor.min.js')}}"></script>
+        <script src="{{asset('/assets/js/theia-sticky-sidebar.min.js')}}"></script>
+        <script src="{{asset('/assets/js/jquery.magnific-popup.min.js')}}"></script>
+        <script src="{{asset('/assets/owl-carousel/owl.carousel.min.js')}}"></script>
+        <script src="{{asset('/assets/js/modernizr.custom.js')}}"></script>
+        <script src="{{asset('/assets/js/jquery.gridrotator.min.js')}}"></script>
+        <script src="{{asset('/assets/js/parallax-background.min.js')}}"></script>
+        <script src="{{asset('/assets/js/jquery.simpleSocialShare.min.js')}}"></script>
+        <script src="{{asset('/assets/js/jquery.fluidbox.min.js')}}"></script>
+        <script src="{{asset('/assets/js/retina.min.js')}}"></script>
+        <script src="{{asset('/assets/js/jquery.shuffle.min.js')}}"></script>
+        <script src="{{asset('/assets/js/readingTime.min.js')}}"></script>
+        <script src="{{asset('/assets/js/custom.js')}}"></script>
+        <script>
+            var masonry = new Macy({
+                container: '#blog-grid',
+                trueOrder: false,
+                waitForImages: false,
+                useOwnImageLoader: false,
+                debug: true,
+                mobileFirst: true,
+                columns: 1,
+                margin: 30,
+                breakAt: {
+                    1200: 2,
+                    992: 2,
+                    768: 2,
+                    480: 2
+                }
+            });
+        </script>
 </body>
 </html>
