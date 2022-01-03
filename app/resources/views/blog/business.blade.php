756056bb1de3e0a34ae35dc02ab14f60bba92f59
Michael Ozoudeh <jesmikky@gmail.com>
2022-01-03 14:26:29 +0100

    updates

--- app/resources/views/blog/business.blade.php
+++ app/resources/views/blog/business.blade.php
@@ -142,109 +142,50 @@
                             </div>
                               <div class="media meida-md">
                                 <div style="width:100px" class="media-left" >
-                                    <a href="#"><img src="{{asset('assets/img/12.png')}}" class="media-object" alt=""></a>
+                                    <a target="_blank" href="https://www.blockchain.com/"><img src="{{asset('assets/img/10.png')}}" class="media-object" alt=""></a>
                                 </div>
                                 <!-- /.Post image -->
                                 <div class="media-body">
                                     <div class="post-header">
-                                        <h3 class="media-heading"><a href="#">Bitfinex</a></h3>
+                                        <h3 class="media-heading"><a target="_blank" href="https://www.blockchain.com/">Blockchain.com</a></h3>
                                         <!-- /.Post meta -->
                                     </div>
-                                    <p>Bitfinex is a cryptocurrency exchange founded in 2012, Bitfinex has always been at the forefront of technological innovation in digital asset trading..</p>
+                                    <p>Blockchain.com got its start as an early pioneer of key infrastructure for the bitcoin community. First, with a Blockchain Explorer that enabled anyone to not only examine transactions and study the blockchain, but an API that enabled companies to build on Bitcoin.</p>
                                     <div class="element-block">
-                                        <a target="_blank" href="https://www.bitfinex.com/" class="btn link-btn btn-outline btn-rounded">Visit Site &#8702;</a></div>
+                                        <a target="_blank" href="https://www.blockchain.com/" class="btn link-btn btn-outline btn-rounded">Visit Site &#8702;</a></div>
                                 </div>
                             </div>
                               <div class="media meida-md">
                                 <div style="width:100px" class="media-left" >
-                                    <a href="#"><img src="{{asset('assets/img/12.png')}}" class="media-object" alt=""></a>
+                                    <a target="_blank" href="https://www.coincorner.com/"><img src="{{asset('assets/img/12.png')}}" class="media-object" alt=""></a>
                                 </div>
                                 <!-- /.Post image -->
                                 <div class="media-body">
                                     <div class="post-header">
-                                        <h3 class="media-heading"><a href="#">Bitfinex</a></h3>
+                                        <h3 class="media-heading"><a target="_blank" href="https://www.coincorner.com/">CoinCorner Ltd </a></h3>
                                         <!-- /.Post meta -->
                                     </div>
-                                    <p>Bitfinex is a cryptocurrency exchange founded in 2012, Bitfinex has always been at the forefront of technological innovation in digital asset trading..</p>
+                                    <p>CoinCorner Ltd (129003C), Level 7, Victory House, Prospect Hill, Douglas, Isle of Man, IM1 1EQ is registered with the Isle of Man Financial Services Authority under the Designated Business Act 2015 and with the Isle of Man Office of Fair Trading as a MoneyLender under the Moneylenders Act 1991.</p>
                                     <div class="element-block">
-                                        <a target="_blank" href="https://www.bitfinex.com/" class="btn link-btn btn-outline btn-rounded">Visit Site &#8702;</a></div>
+                                        <a target="_blank" href="https://www.coincorner.com/" class="btn link-btn btn-outline btn-rounded">Visit Site &#8702;</a></div>
                                 </div>
                             </div>
                               <div class="media meida-md">
                                 <div style="width:100px" class="media-left" >
-                                    <a href="#"><img src="{{asset('assets/img/12.png')}}" class="media-object" alt=""></a>
+                                    <a target="_blank" href="https://zenithcapital.cc/"><img src="{{asset('assets/img/13.png')}}" class="media-object" alt=""></a>
                                 </div>
                                 <!-- /.Post image -->
                                 <div class="media-body">
                                     <div class="post-header">
-                                        <h3 class="media-heading"><a href="#">Bitfinex</a></h3>
+                                        <h3 class="media-heading"><a target="_blank" href="https://zenithcapital.cc/">ZenithCapital CC</a></h3>
                                         <!-- /.Post meta -->
                                     </div>
-                                    <p>Bitfinex is a cryptocurrency exchange founded in 2012, Bitfinex has always been at the forefront of technological innovation in digital asset trading..</p>
+                                    <p>ZenithCapital is your partner for Trading online through our premium online brokerage. As a provider for online trading and investment services we offer you reliability that meets the highest standards.</p>
                                     <div class="element-block">
-                                        <a target="_blank" href="https://www.bitfinex.com/" class="btn link-btn btn-outline btn-rounded">Visit Site &#8702;</a></div>
+                                        <a target="_blank" href="https://zenithcapital.cc/" class="btn link-btn btn-outline btn-rounded">Visit Site &#8702;</a></div>
                                 </div>
                             </div>
-                              <div class="media meida-md">
-                                <div style="width:100px" class="media-left" >
-                                    <a href="#"><img src="{{asset('assets/img/12.png')}}" class="media-object" alt=""></a>
-                                </div>
-                                <!-- /.Post image -->
-                                <div class="media-body">
-                                    <div class="post-header">
-                                        <h3 class="media-heading"><a href="#">Bitfinex</a></h3>
-                                        <!-- /.Post meta -->
-                                    </div>
-                                    <p>Bitfinex is a cryptocurrency exchange founded in 2012, Bitfinex has always been at the forefront of technological innovation in digital asset trading..</p>
-                                    <div class="element-block">
-                                        <a target="_blank" href="https://www.bitfinex.com/" class="btn link-btn btn-outline btn-rounded">Visit Site &#8702;</a></div>
-                                </div>
-                            </div>
-                              <div class="media meida-md">
-                                <div style="width:100px" class="media-left" >
-                                    <a href="#"><img src="{{asset('assets/img/12.png')}}" class="media-object" alt=""></a>
-                                </div>
-                                <!-- /.Post image -->
-                                <div class="media-body">
-                                    <div class="post-header">
-                                        <h3 class="media-heading"><a href="#">Bitfinex</a></h3>
-                                        <!-- /.Post meta -->
-                                    </div>
-                                    <p>Bitfinex is a cryptocurrency exchange founded in 2012, Bitfinex has always been at the forefront of technological innovation in digital asset trading..</p>
-                                    <div class="element-block">
-                                        <a target="_blank" href="https://www.bitfinex.com/" class="btn link-btn btn-outline btn-rounded">Visit Site &#8702;</a></div>
-                                </div>
-                            </div>
-                              <div class="media meida-md">
-                                <div style="width:100px" class="media-left" >
-                                    <a href="#"><img src="{{asset('assets/img/12.png')}}" class="media-object" alt=""></a>
-                                </div>
-                                <!-- /.Post image -->
-                                <div class="media-body">
-                                    <div class="post-header">
-                                        <h3 class="media-heading"><a href="#">Bitfinex</a></h3>
-                                        <!-- /.Post meta -->
-                                    </div>
-                                    <p>Bitfinex is a cryptocurrency exchange founded in 2012, Bitfinex has always been at the forefront of technological innovation in digital asset trading..</p>
-                                    <div class="element-block">
-                                        <a target="_blank" href="https://www.bitfinex.com/" class="btn link-btn btn-outline btn-rounded">Visit Site &#8702;</a></div>
-                                </div>
-                            </div>
-                              <div class="media meida-md">
-                                <div style="width:100px" class="media-left" >
-                                    <a href="#"><img src="{{asset('assets/img/12.png')}}" class="media-object" alt=""></a>
-                                </div>
-                                <!-- /.Post image -->
-                                <div class="media-body">
-                                    <div class="post-header">
-                                        <h3 class="media-heading"><a href="#">Bitfinex</a></h3>
-                                        <!-- /.Post meta -->
-                                    </div>
-                                    <p>Bitfinex is a cryptocurrency exchange founded in 2012, Bitfinex has always been at the forefront of technological innovation in digital asset trading..</p>
-                                    <div class="element-block">
-                                        <a target="_blank" href="https://www.bitfinex.com/" class="btn link-btn btn-outline btn-rounded">Visit Site &#8702;</a></div>
-                                </div>
-                            </div>
+                             
                         </main>
                         <aside class="col-xs-12 col-sm-4 col-md-3 rightSidebar">
                             <div class="latest_post_widget">
@@ -255,82 +196,53 @@
                                 <!--  /.End of title -->
                                 <div class="media latest_post">
                                     <a class="media-left" href="#">
-                                        <img src="assets/img/100x70-1.jpg" class="media-object" alt="">
+                                        <img src="{{asset('assets/img/10.png')}}" class="media-object" alt="">
                                     </a>
                                     <div class="media-body">
-                                        <h6 class="media-heading"><a href="#">The <em>Best Street-Style</em> Pics Copenhagen</a></h6>
-                                        <div class="entry-meta">
-                                            <span class="entry-date"><i class="fa fa-calendar-o" aria-hidden="true"></i><time datetime="2018-01-21T19:00">Jan 21, 2018</time></span>
-                                        </div>
+                                        <h6 class="media-heading"><a href="#">Blockchain.com</a></h6>
+                                           <div class="element-block">
+                                        <a target="_blank" href="https://Blockchain.com/" class="link-btn  btn-rounded">Visit Site &#8702;</a></div>
                                     </div>
                                 </div>
-                                <!-- /.latest post -->
-                                <div class="media latest_post">
+                                 <div class="media latest_post">
                                     <a class="media-left" href="#">
-                                        <img src="assets/img/100x70-2.jpg" class="media-object" alt="">
+                                        <img src="{{asset('assets/img/15.png')}}" class="media-object" alt="">
                                     </a>
                                     <div class="media-body">
-                                        <h6 class="media-heading"><a href="#">The <em>Best Street-Style</em> Pics Copenhagen</a></h6>
-                                        <div class="entry-meta">
-                                            <span class="entry-date"><i class="fa fa-calendar-o" aria-hidden="true"></i><time datetime="2018-01-21T19:00">Jan 21, 2018</time></span>
-                                        </div>
+                                        <h6 class="media-heading"><a href="#">BTC.com</a></h6>
+                                           <div class="element-block">
+                                        <a target="_blank" href="https://btc.com/" class=" link-btn  btn-rounded">Visit Site &#8702;</a></div>
                                     </div>
                                 </div>
-                                <!-- /.latest post -->
-                                <div class="media latest_post">
+                                 <div class="media latest_post">
                                     <a class="media-left" href="#">
-                                        <img src="assets/img/100x70-3.jpg" class="media-object" alt="">
+                                        <img src="{{asset('assets/img/16.png')}}" class="media-object" alt="">
                                     </a>
                                     <div class="media-body">
-                                        <h6 class="media-heading"><a href="#">The <em>Best Street-Style</em> Pics Copenhagen</a></h6>
-                                        <div class="entry-meta">
-                                            <span class="entry-date"><i class="fa fa-calendar-o" aria-hidden="true"></i><time datetime="2018-01-21T19:00">Jan 21, 2018</time></span>
-                                        </div>
+                                        <h6 class="media-heading"><a href="#">Ripple.com</a></h6>
+                                           <div class="element-block">
+                                        <a target="_blank" href="https://Ripple.com/" class="link-btn  btn-rounded">Visit Site &#8702;</a></div>
                                     </div>
                                 </div>
-                                <!-- /.latest post -->
-                            </div>
-                            <!-- /.End of latest post -->
-                            <div class="social_share_btn">
-                                <div class="title-holder">
-                                    <h3 class="title">Subscribe</h3>
-                                    <span class="title-shape title-shape-dark"></span>
+                                 <div class="media latest_post">
+                                    <a class="media-left" href="#">
+                                        <img src="{{asset('assets/img/18.png')}}" class="media-object" alt="">
+                                    </a>
+                                    <div class="media-body">
+                                        <h6 class="media-heading"><a href="#">Crypto.com</a></h6>
+                                           <div class="element-block">
+                                        <a target="_blank" href="https://Crypto.com" class=" link-btn btn-outline btn-rounded">Visit Site &#8702;</a></div>
+                                    </div>
                                 </div>
-                                <!--  /.End of title -->
-                                <ul>
-                                    <li class="li-facebook"><a href="#" target="_blank"><i class="fa fa-facebook"></i> Facebook</a></li>
-                                    <li class="li-twitter"><a href="#" target="_blank"><i class="fa fa-twitter"></i> Twitter</a></li>
-                                    <li class="li-google-plus"><a href="#" target="_blank"><i class="fa fa-google-plus"></i> Google +</a></li>
-                                    <li class="li-pinterest"><a href="#" target="_blank"><i class="fa fa-pinterest-p"></i> Pinterest</a></li>
-                                </ul>
+                               
                             </div>
+                       
                             <!-- /.End of subscribe -->
                             <div class="banner-content">
-                                <img src="assets/img/ad-300x250-1.png" class="img-responsive center-block" alt="">
+                                <img src="{{asset('assets/img/370x300-1.jpg')}}" class="img-responsive center-block" alt="">
                             </div>
                             <!-- /.End of banner content -->
-                            <div class="category_widget">
-                                <div class="title-holder">
-                                    <h3 class="title">Category</h3>
-                                    <span class="title-shape title-shape-dark"></span>
-                                </div>
-                                <!--  /.End of title -->
-                                <a class="category" href="#">
-                                    <figure><img src="assets/img/category-1.jpg" class="img-responsive" alt=""></figure>
-                                    <div class="category_name">Fashion</div>
-                                </a>
-                                <!-- /.End of category -->
-                                <a class="category" href="#">
-                                    <figure><img src="assets/img/category-2.jpg" class="img-responsive" alt=""></figure>
-                                    <div class="category_name">Travel</div>
-                                </a>
-                                <!-- /.End of category -->
-                                <a class="category" href="#">
-                                    <figure><img src="assets/img/category-3.jpg" class="img-responsive" alt=""></figure>
-                                    <div class="category_name">Life Style</div>
-                                </a>
-                                <!-- /.End of category -->
-                            </div>
+                            
                             <!-- /.End of category widget -->
                         </aside>
                     </div>
