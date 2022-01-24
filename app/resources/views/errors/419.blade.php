f7a2fa851e37dbd3316ec5649425d7478def40b4
Michael Ozoudeh <jesmikky@gmail.com>
2022-01-24 02:50:34 +0100

    updates

---
+++ app/resources/views/errors/419.blade.php
@@ -0,0 +1,26 @@
+@extends('layouts.app')
+
+@section('content')
+
+
+     <div class="page-content">
+                    <div class="middle-box">
+                        <div class="row">
+                            <div class="col-sm-12">
+                                <div class="error-text">
+                                    <h1>4<span class="error bounce">0</span><span>4</span></h1>
+                                    <h3><span>Page</span><br class="hidden-xs"> Not Found</h3>
+                                </div>
+                            </div>
+                        </div>
+                        <div class="row">
+                            <div class="col-sm-12">
+                                <div class="error-desc">
+                                    <p>Sorry, but the page you are looking for has note been found. Try checking the URL for error, then hit the 
+                                        refresh button on your browser or try found something else in our app.</p>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+@endsection
