361bef40510fcf3472691fc5551910009df305b5
Michael Ozoudeh <jesmikky@gmail.com>
2022-01-25 03:32:59 +0100

    updates

--- app/resources/views/layouts/app.blade.php
+++ app/resources/views/layouts/app.blade.php
@@ -68,7 +68,7 @@
                 <button type="button" class="navbar-toggle" data-toggle="collapse" data-target="#navbar-menu">
                     <i class="fa fa-bars"></i>
                 </button>
-                <a class="navbar-brand" href="{{route('index')}}"><p style="color:#fff; font-weight:bolder; margin-top:10px"> CryptoNewsUpdate<span style="font-size:25px; font-weight:bold; background:#fff; color:#000; border-radius:5px">24</span> </p></a>
+                <a class="navbar-brand" href="{{route('index')}}"><p style="color:#fff; font-weight:bolder; margin-top:10px"> CryptoNewsUpdate<span style="font-size:25px; font-weight:bold; background:#d17c78; color:#fff; border-radius:5px">24</span> </p></a>
             </div>
             <div class="collapse navbar-collapse" id="navbar-menu">
                 <ul class="nav navbar-nav navbar-center" data-in="navFadeInDown" data-out="navFadeOutUp">
